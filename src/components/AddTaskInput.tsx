d5be855a528ee8ef2f2c389ed1852d733c81cf28
Dan <vareladanmarj@gmail.com>
2022-12-30 11:21:22 +0800

    enable adding tasks

--- src/components/AddTaskInput.tsx
+++ src/components/AddTaskInput.tsx
@@ -1,14 +1,47 @@
-import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Box,
+} from '@chakra-ui/react';
+import useTaskStore from '@stores/task';
+import { useState } from 'react';
 
 export default function AddTaskInput() {
+  const [body, setBody] = useState('');
+  const { addTask } = useTaskStore();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBody(e.target.value);
+  };
+
+  const handleAddTask = (
+    e: React.FormEvent<HTMLFormElement & HTMLDivElement>
+  ) => {
+    if ('preventDefault' in e) {
+      e.preventDefault();
+    }
+    addTask({ body });
+    setBody('');
+  };
+
   return (
-    <InputGroup size="md" maxW="container.sm" w="full">
-      <Input pr={5} type="text" placeholder="Type here" />
-      <InputRightElement w={20} mr={1}>
-        <Button size="sm" colorScheme="blue">
-          Add Task
-        </Button>
-      </InputRightElement>
-    </InputGroup>
+    <Box w="full" as="form" onSubmit={handleAddTask}>
+      <InputGroup size="md" maxW="container.sm" w="full">
+        <Input
+          pr={5}
+          type="text"
+          placeholder="Type here"
+          value={body}
+          onChange={handleChange}
+        />
+        <InputRightElement w={20} mr={1}>
+          <Button size="sm" colorScheme="blue" type="submit">
+            Add Task
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </Box>
   );
 }
